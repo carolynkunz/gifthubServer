ac761a2563b1d00f2ea23a9dca367ae5df6e52a9
carolynkunz <carolyn.delbeccaro@gmail.com>
2016-12-21 14:30:13 -0800

    server running

---
+++ routes/users.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/* eslint-disable new-cap, no-console, prefer-const, max-len*/
+
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+router.post('/api/users', (req, res, next) => {
+  let { firstName, lastName, email, password, username } = req.body;
+
+  console.log(req.body);
+
+  if (!firstName || !firstName.trim()) {
+    return next(boom.create(400, 'First Name must not be blank'));
+  }
+
+  if (!lastName || !lastName.trim()) {
+    return next(boom.create(400, 'Last Name must not be blank'));
+  }
+
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!username || !username.trim()) {
+    return next(boom.create(400, 'Username must not be blank'));
+  }
+
+  if (!password || password.length < 8) {
+    return next(boom.create(
+      400,
+      'Password must be at least 8 characters long'
+    ));
+  }
+
+  knex('users')
+    .where('username', username)
+    .first()
+    .then((exists) => {
+      if (exists) {
+        throw boom.create(400, 'Username already exists');
+      }
+      console.log(username, password);
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
+      firstName = firstName.toLowerCase();
+      lastName = lastName.toLowerCase();
+      email = email.toLowerCase();
+      username = username.toLowerCase();
+      const insertUser = { firstName, lastName, email, hashedPassword, username };
+
+      return knex('users')
+        .insert(decamelizeKeys(insertUser), '*');
+    })
+    .then((rows) => {
+      const user = camelizeKeys(rows[0]);
+
+      delete user.hashedPassword;
+
+      const expiry = new Date(Date.now() + 1000 * 60 * 60 * 3); // 3 hours
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+        expiresIn: '3h'
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        expires: expiry,
+        secure: router.get('env') === 'production'
+      });
+
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
